08ccf00b0f630cce0e8866c573ddf627d17192bf
Max Baer <rmaxbaer@gmail.com>
2020-11-14 11:53:56 -0700

    html for templates

--- FinalProject/finalproject/settings.py
+++ FinalProject/finalproject/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'moviereviews.apps.MoviereviewsConfig',
 ]
-
+ 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
